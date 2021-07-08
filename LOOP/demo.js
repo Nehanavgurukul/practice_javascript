2de21b2fc6536430ae3b1996295e80693d13f171
Nehanavgurukul <nehay19@navgurukul.org>
2021-07-08 10:43:00 +0530

    loop

--- LOOP/demo.js
+++ LOOP/demo.js
@@ -6,3 +6,34 @@
 //while,do while,for,for in,for of
 
 
+let i=2
+do{
+   console.log(i);
+}
+while (i >10)
+ 
+ 
+while(i>10){
+   console.log(i);
+}
+
+
+//what is for in loop?
+//Ans=>>for in loop is a special type of loop that iterates over the object or an array of elements.
+ 
+
+
+let name=["komal","shweta","rani","swati","mahi","shanti","sarmistha"]
+for(let lname of name){
+   console.log(lname);
+}
+
+/*Output: 
+komal
+shweta
+rani
+swati
+mahi	
+shanti
+sarmistha
+*/
